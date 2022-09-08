38ba4c82a5d99aaf8a75ad1d54a0e9fb740a9ecf
wang <39637005+waterrespect@users.noreply.github.com>
2022-09-08 18:18:00 +0800

    Update test.java

--- service/service-oss/src/test/java/com/wang/yygh/oss/test.java
+++ service/service-oss/src/test/java/com/wang/yygh/oss/test.java
@@ -10,7 +10,7 @@ public class test {
         // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
         String endpoint = "https://oss-cn-hangzhou.aliyuncs.com";
         // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        String accessKeyId = "LTAI5tSD8ZNAdR9rh4X9Z9Rw";
+        String accessKeyId ="";
         String accessKeySecret = "qEbLBSrnQwWGjz9AZWODVHXRkSvnE1";
         // 填写Bucket名称，例如examplebucket。
         String bucketName = "wang-pic";
